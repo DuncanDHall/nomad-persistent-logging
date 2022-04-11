7601d9693ba56ffbea08f5f5f05062f564b5b5b2
Duncan Hall <duncandhall@gmail.com>
2022-04-11 17:23:13 -0400

    simplify to just task spec

--- log_shipper_start.sh
+++ log_shipper_start.sh
@@ -1,11 +1,11 @@
 mkdir /etc/cron.d
-cd /etc/cron.d
-wget https://git.archive.org/services/persistent-logging/-/raw/main/log_shipper.sh
-
-cd /
-# between 1 and 3 am
-randomminute=$(($RANDOM % 60))
-randomhour=$((1 + $RANDOM % 2))
-# echo -e "$randomminute $randomhour * * * sh /etc/cron.d/log_shipper.sh $1 >> /var/log/cron.log 2>&1\n" >> /etc/crontabs/root
-echo -e "* * * * * sh /etc/cron.d/log_shipper.sh $1 >> /var/log/cron.log 2>&1\n" >> /etc/crontabs/root
-crontab /etc/crontabs/root
+# cd /etc/cron.d
+# wget https://git.archive.org/services/persistent-logging/-/raw/main/log_shipper.sh
+#
+# cd /
+# # between 1 and 3 am
+# randomminute=$(($RANDOM % 60))
+# randomhour=$((1 + $RANDOM % 2))
+# # echo -e "$randomminute $randomhour * * * sh /etc/cron.d/log_shipper.sh $1 >> /var/log/cron.log 2>&1\n" >> /etc/crontabs/root
+# echo -e "* * * * * sh /etc/cron.d/log_shipper.sh $1 >> /var/log/cron.log 2>&1\n" >> /etc/crontabs/root
+# crontab /etc/crontabs/root
