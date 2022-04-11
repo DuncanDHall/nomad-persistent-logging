517fa41137bda72efffb56565e8bfea0d8f3c200
Duncan Hall <duncandhall@gmail.com>
2022-04-11 16:49:22 -0400

    make cron more frequent for confirmation/testing

--- log_shipper_start.sh
+++ log_shipper_start.sh
@@ -6,5 +6,6 @@ cd /
 # between 1 and 3 am
 randomminute=$(($RANDOM % 60))
 randomhour=$((1 + $RANDOM % 2))
-echo -e "$randomminute $randomhour * * * sh /etc/cron.d/log_shipper.sh $1 >> /var/log/cron.log 2>&1\n" >> /etc/crontabs/root
+# echo -e "$randomminute $randomhour * * * sh /etc/cron.d/log_shipper.sh $1 >> /var/log/cron.log 2>&1\n" >> /etc/crontabs/root
+echo -e "* * * * * sh /etc/cron.d/log_shipper.sh $1 >> /var/log/cron.log 2>&1\n" >> /etc/crontabs/root
 crontab /etc/crontabs/root
