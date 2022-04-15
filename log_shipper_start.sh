c3cf4939d9d89a9e2b7f676ca2c6ff319463bcd1
Duncan Hall <duncandhall@gmail.com>
2022-04-15 15:40:29 -0400

    fix crontab location

--- log_shipper_start.sh
+++ log_shipper_start.sh
@@ -5,7 +5,7 @@ cd /
 # randomhour=$((1 + $RANDOM % 2))
 # echo -e "$randomminute $randomhour * * * sh /etc/cron.d/log_shipper.sh $1 >> /var/log/cron.log 2>&1\n" >> /etc/crontabs/root
 echo -e "* * * * * sh /local/log_shipper.sh $1 >> /var/log/cron.log 2>&1\n" >> /local/logger_crontab
-crontab /etc/logger_crontab
+crontab /local/logger_crontab
 
 sleep 3600
 echo "hi, I'm here, you need ot sleep"
