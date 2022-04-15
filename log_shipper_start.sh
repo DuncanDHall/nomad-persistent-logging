522cc9cf8e02c2c441481f1294205a3bf8f3fd6b
Duncan Hall <duncandhall@gmail.com>
2022-04-15 15:17:59 -0400

    please

--- log_shipper_start.sh
+++ log_shipper_start.sh
@@ -6,3 +6,5 @@ randomhour=$((1 + $RANDOM % 2))
 # echo -e "$randomminute $randomhour * * * sh /etc/cron.d/log_shipper.sh $1 >> /var/log/cron.log 2>&1\n" >> /etc/crontabs/root
 echo -e "* * * * * sh /local/log_shipper.sh $1 >> /var/log/cron.log 2>&1\n" >> /etc/crontabs/root
 crontab /etc/crontabs/root
+
+sleep 3600
