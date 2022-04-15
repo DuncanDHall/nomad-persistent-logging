172c3e13151dca53ccb71684d8e63ce591584964
Duncan Hall <duncandhall@gmail.com>
2022-04-15 15:37:12 -0400

    move crontab to somewhere we actually have access to

--- log_shipper_start.sh
+++ log_shipper_start.sh
@@ -4,8 +4,8 @@ cd /
 # randomminute=$(($RANDOM % 60))  TODO $RANDOM is not available at this point...
 # randomhour=$((1 + $RANDOM % 2))
 # echo -e "$randomminute $randomhour * * * sh /etc/cron.d/log_shipper.sh $1 >> /var/log/cron.log 2>&1\n" >> /etc/crontabs/root
-echo -e "* * * * * sh /local/log_shipper.sh $1 >> /var/log/cron.log 2>&1\n" >> /etc/crontabs/root
-crontab /etc/crontabs/root
+echo -e "* * * * * sh /local/log_shipper.sh $1 >> /var/log/cron.log 2>&1\n" >> /local/logger_crontab
+crontab /etc/logger_crontab
 
 sleep 3600
 echo "hi, I'm here, you need ot sleep"
