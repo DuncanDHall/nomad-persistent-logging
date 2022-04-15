63bbeb91fcc222df25615a80fcfaf6b8f6c317c7
Duncan Hall <duncandhall@gmail.com>
2022-04-15 15:27:49 -0400

    remove random

--- log_shipper_start.sh
+++ log_shipper_start.sh
@@ -1,8 +1,8 @@
 
 cd /
 # between 1 and 3 am
-randomminute=$(($RANDOM % 60))
-randomhour=$((1 + $RANDOM % 2))
+# randomminute=$(($RANDOM % 60))  TODO $RANDOM is not available at this point...
+# randomhour=$((1 + $RANDOM % 2))
 # echo -e "$randomminute $randomhour * * * sh /etc/cron.d/log_shipper.sh $1 >> /var/log/cron.log 2>&1\n" >> /etc/crontabs/root
 echo -e "* * * * * sh /local/log_shipper.sh $1 >> /var/log/cron.log 2>&1\n" >> /etc/crontabs/root
 crontab /etc/crontabs/root
